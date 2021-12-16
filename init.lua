cc38d26e1f6c65d3132602d733dd2d8d51a7ff7e
Zachariah Ngonyani <zachariahn@gmail.com>
2021-12-16 17:33:44 +0300

    Add more configuration to init.vim

--- init.lua
+++ init.lua
@@ -1,8 +1,22 @@
 -- switch to packer plugin manager  instead of plug
 -- switch to packer plugin manager  instead of plug
 require("user.options")
+require("user.settings")
 require("user.plugins")
+
 require("user.colorscheme")
 
 require("user.tree-sitter")
-require("user.settings")
+require("user.whichkey")
+require("user.comment")
+require("user.term")
+require("user.vim-project")
+require("user.impatient")
+require("user.vim-zen")
+require("user.alpha")
+require("user.telescope")
+require("user.renamer")
+require("user.lsp")
+require("user.autocmd")
+require("user.colorizer")
+require("user.spectre")
